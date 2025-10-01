e9ac782bca92740688d277c720394752ac6af282
alvexx45 <bernardoalves222@gmail.com>
2025-10-01 18:07:36 -0300

    ClienteDAO

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,5 +1,26 @@
 package dao;
+import model.Cliente;
 
 public class ClienteDAO {
-    
+    private Arquivo<Cliente> arqClientes;
+
+    public ClienteDAO() throws Exception {
+        arqClientes = new Arquivo<>("clientes", Cliente.class.getConstructor());
+    }
+
+    public boolean incluirCliente(Cliente cliente) throws Exception {
+        return arqClientes.create(cliente) > 0;
+    }
+
+    public boolean alterarCliente(Cliente cliente) throws Exception {
+        return arqClientes.update(cliente);
+    }
+
+    public boolean excluirCliente(int id) throws Exception {
+        return arqClientes.delete(id);
+    }
+
+    public Cliente buscarCliente(int id) throws Exception {
+        return arqClientes.read(id);
+    }
 }
