77517578c6f4fe3e42f9284230c9da6fb6ba5538
alvexx45 <bernardoalves222@gmail.com>
2025-10-01 14:04:40 -0300

    create class Agendar

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -17,5 +17,13 @@ class Cliente {
         this.telefones = telefones;
     }
 
-    
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public void setNome(String nome) { this.nome = nome; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelefones(String[] telefones) { this.telefones = telefones; }
+
+    public String getCpf() { return cpf; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String[] getTelefones() { return telefones; }
 }
