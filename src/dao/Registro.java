5f71debf3c151bf3a3eac5e5642ef3fe56f6a52e
alvexx45 <bernardoalves222@gmail.com>
2025-10-01 17:48:48 -0300

    Registro

---
+++ src/dao/Registro.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.io.IOException;
+
+public interface Registro {
+    public void setId(int i);
+    public int getId();
+    public byte[] toByteArray() throws IOException;
+    public void fromByteArray(byte[] b) throws IOException;
+}
