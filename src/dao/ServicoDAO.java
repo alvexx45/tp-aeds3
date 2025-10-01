87fbd2cd46df32aeafd3a57bfa256eaebe5dbe6f
alvexx45 <bernardoalves222@gmail.com>
2025-10-01 18:17:40 -0300

    ServicoDAO

--- src/dao/ServicoDAO.java
+++ src/dao/ServicoDAO.java
@@ -1,5 +1,26 @@
 package dao;
+import model.Servico;
 
 public class ServicoDAO {
-    
+    private Arquivo<Servico> arqServicos;
+
+    public ServicoDAO() throws Exception {
+        arqServicos = new Arquivo<>("servicos", Servico.class.getConstructor());
+    }
+
+    public boolean incluirServico(Servico servico) throws Exception {
+        return arqServicos.create(servico) > 0;
+    }
+
+    public boolean alterarServico(Servico servico) throws Exception {
+        return arqServicos.update(servico);
+    }
+
+    public boolean excluirServico(int id) throws Exception {
+        return arqServicos.delete(id);
+    }
+
+    public Servico buscarServico(int id) throws Exception {
+        return arqServicos.read(id);
+    }
 }
