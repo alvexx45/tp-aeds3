48d4116b8b6a26fc994ce75469eec70e3638f530
alvexx45 <bernardoalves222@gmail.com>
2025-10-01 18:13:40 -0300

    PetDAO

--- src/dao/PetDAO.java
+++ src/dao/PetDAO.java
@@ -1,5 +1,26 @@
 package dao;
+import model.Pet;
 
 public class PetDAO {
-    
+    private Arquivo<Pet> arqPets;
+
+    public PetDAO() throws Exception {
+        arqPets = new Arquivo<>("pets", Pet.class.getConstructor());
+    }
+
+    public boolean incluirPet(Pet pet) throws Exception {
+        return arqPets.create(pet) > 0;
+    }
+
+    public boolean alterarPet(Pet pet) throws Exception {
+        return arqPets.update(pet);
+    }
+
+    public boolean excluirPet(int id) throws Exception {
+        return arqPets.delete(id);
+    }
+
+    public Pet buscarPet(int id) throws Exception {
+        return arqPets.read(id);
+    }
 }
