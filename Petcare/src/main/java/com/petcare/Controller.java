dfb41d6b6f73f35e97d4c8d3800d9c25fef33aeb
Educa0198 <vieiraeduardo2006@gmail.com>
2025-10-14 03:00:07 -0300

    Atualização do esqueleto

--- Petcare/src/main/java/com/petcare/Controller.java
+++ Petcare/src/main/java/com/petcare/Controller.java
@@ -1,14 +1,59 @@
 package com.petcare;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private AnchorPane rootPane;
+
+
+
+    public void switchtoGerenciarMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/petcare/view.fxml"));
+        stage = (Stage) rootPane.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public void switchtoGerenciarClientes(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/com/petcare/GerenciarClientes.fxml"));
+        stage = (Stage)rootPane.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchtoGerenciarPets(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/petcare/GerenciarPets.fxml"));
+        stage = (Stage)rootPane.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchtoGerenciarServicos(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/com/petcare/GerenciarServicos.fxml"));
+        stage = (Stage)rootPane.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
+
 }
